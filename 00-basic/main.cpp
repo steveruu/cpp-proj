0788075876924cbcf06e00c06da0df9ba3ce6ada
stepan <steveruu@stvr.cz>
2024-10-13 01:49:02 +0200

    structs, classes

--- 00-basic/main.cpp
+++ 00-basic/main.cpp
@@ -6,11 +6,31 @@ int main()
     std::cout << "Hello World!" << std::endl;
 
     // dynamic array, can change its size (not its type)
-    std::vector<std::string> students = {"UX", "Anonym"};
-    students.push_back("adyze");
+    std::vector<std::string> strings = {"UX", "Anonym"};
+    strings.push_back("adyze");
 
-    for (std::string student : students)
+    // forof
+    for (std::string string : strings)
     {
-        std::cout << student << std::endl;
+        std::cout << string << std::endl;
     }
+
+    // struct syntax
+    struct
+    {
+        std::string name = "MH";
+        std::string field = "React Developer";
+    } sedmilhar;
+
+    class student
+    {
+    public:
+        std::string name;
+        int phoneNumber;
+        student() { std::cout << "student byl zalozen" << std::endl; };
+    };
+
+    // classes
+    student ja;
+    ja.name = "steveruu";
 }
